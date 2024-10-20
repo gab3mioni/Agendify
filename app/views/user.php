992acf40c5e94b5713e05470eefb4e59267a9ddc
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-10-20 17:59:34 -0300

    Atualizando user.php

--- app/views/user.php
+++ app/views/user.php
@@ -7,7 +7,12 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-
+<div class="container mt-5">
+    <h1 class="text-center">Bem-vindo à User!</h1>
+    <div class="text-center">
+        <a href="home" class="btn btn-primary mt-3">Ir para Home</a>
+    </div>
+</div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
