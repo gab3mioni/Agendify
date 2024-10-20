7096c19063ee95779b00a24da54b0569bff4656f
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-10-20 18:00:53 -0300

    Criando RegisterModel.php

---
+++ app/models/RegisterModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+use PDO;
+
+class RegisterModel {
+
+    private $pdo;
+
+    public function  __construct()
+    {
+        global $pdo;
+        $this->pdo = $pdo;
+    }
+
+    public function registerUser($name, $email, $password)
+    {
+        $stmt = $this->pdo->prepare("INSERT INTO users (name, email, password) VALUES (:name, :email, :password)");
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':password', $password);
+        return $stmt->execute();
+    }
+
+}
