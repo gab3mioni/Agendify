566de6cb471ea9322a5d54b69bd71f5b0aabc50e
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-10-20 23:47:47 -0300

    Alterando Dashboard
    Estilizando melhor a dashboard

--- app/views/dashboard.php
+++ app/views/dashboard.php
@@ -4,22 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Agendify</title>
-    <link href="assets/css/dashboard.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/dashboard.css" rel="stylesheet">
 </head>
 <body>
 
 <nav class="sidebar">
     <a href="#">Dashboard</a>
     <a href="#">Agenda</a>
+    <a href="profile">Editar Perfil</a>
     <a href="#">Configurações</a>
     <a href="logout">Sair</a>
 </nav>
 
-<div class="container">
-        <div class="mt-5">
-            <h1>Bem-vindo à Dashboard</h1>
-        </div>
+<div class="content">
+    <div class="container mt-5">
+        <h1>Bem-vindo à Dashboard</h1>
+    </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
