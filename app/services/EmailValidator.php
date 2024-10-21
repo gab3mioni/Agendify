35b3bd02198dace26e94bef53b54dca062944fc1
Gabriel Mioni <gabrielmionibastos@gmail.com>
2024-10-21 12:39:44 -0300

    Criando EmailValidator.php

---
+++ app/services/EmailValidator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Services;
+
+class EmailValidator
+{
+    public function validateEmail($email)
+    {
+        $allowedDomains = ['gmail.com', 'hotmail.com', 'yahoo.com'];
+        $emailDomain = substr(strrchr($email, "@"), 1);
+        return in_array($emailDomain, $allowedDomains);
+    }
+}
